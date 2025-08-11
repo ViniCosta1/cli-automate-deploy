803f58b61780bf01ffac3b35e163a45144f641bc
ViniCosta1 <vinicosta154@hotmail.com>
2025-08-10 23:09:17 -0300

    fix: golangci-lint issues fix

--- cmd/root.go
+++ cmd/root.go
@@ -10,7 +10,6 @@ var rootCmd = &cobra.Command{
 	Use:   "dpcli",
 	Short: "A CLI tool for automating deployments",
 	Long:  `dpcli is a command line interface tool that helps automate deployment processes.`,
-	Args: cobra.NoArgs,
 	Run: func(_ *cobra.Command, _ []string) {
 		fmt.Println("Welcome to dpcli!")
 	},
